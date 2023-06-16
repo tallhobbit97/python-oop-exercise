f4cf0c2088b329d856e8177963eacc3f41b8255c
tallhobbit97 <125775467+tallhobbit97@users.noreply.github.com>
2023-06-16 16:58:29 +0000

    oop done

--- serial.py
+++ serial.py
@@ -19,4 +19,36 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start=100):
+        self.start = start
+        self.curr_serial = start
+        self.next_serial = start
+    
+    def generate(self):
+        """
+        Generates a new serial number and increments SerialGenerator.next_serial by one every time it is called.
+        """
+        if self.curr_serial == self.start:
+            self.next_serial += 1
+            self.curr_serial = self.next_serial
+            return self.start
+        else:
+            self.curr_serial = self.next_serial
+            self.next_serial += 1
+            return self.curr_serial
+    
+    def reset(self):
+        """
+        Resets SerialGenerator.curr_serial and SerialGenerator.next_serial back to SerialGenerator.start value
+        """
+        self.curr_serial = self.start
+        self.next_serial = self.start
+        
 
+serial = SerialGenerator(start=100)
+
+print(serial.generate())
+print(serial.generate())
+print(serial.generate())
+print(serial.reset())
+print(serial.generate())
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,47 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, file_path):
+        self.word_list = self.get_list(file_path)
+        print(f"{len(self.word_list)} words read")
+    
+    def get_list(self, file_path):
+        length = random.randint(3, 10)
+        word_list = []
+        file = open(file_path)
+        words = file.read().split()
+
+        i = 0
+        while i < length:
+            word_list.append(random.choice(words))
+            i += 1
+        file.close()
+        return list(set(word_list))
+    
+    def random(self):
+        return self.word_list[random.randint(0, len(self.word_list) - 1)]
+
+wf = WordFinder('words.txt')
+
+print(wf.random())
+print(wf.random())
+print(wf.random())
+
+class RandomWordFinder(WordFinder):
+    def __init__(self, file_path):
+        self.word_list = self.get_list(file_path)
+        print(f"{len(self.word_list)} words read")
+    
+    def get_list(self, file_path):
+        words = list(set(super().get_list(file_path)))
+        for word in words:
+            if word == "" or word.startswith("#"):
+                words.remove(word)
+        return words
+    
+rwf = RandomWordFinder("more_words.txt")
+
+print(rwf.random())
+print(rwf.random())
+print(rwf.random())
